1b1cd683b1cde505539e655772a67574ad2ba3e8
karthikbonagiri <63584081+karthikbonagiri@users.noreply.github.com>
2026-03-21 21:55:22 +0530

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>MASTER MINDS – The Learning Hub 🚀</h1>
+      <p>Website is working successfully</p>
+    </div>
+  );
+}
