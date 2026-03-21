10522ec231e8b94125581611fe747c7ec00a37b4
karthikbonagiri <63584081+karthikbonagiri@users.noreply.github.com>
2026-03-21 22:10:39 +0530

    Delete src/app/page.tsx

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>MASTER MINDS – The Learning Hub 🚀</h1>
-      <p>Website is working successfully</p>
-    </div>
-  );
-}
